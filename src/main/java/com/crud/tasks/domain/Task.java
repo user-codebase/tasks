daca5863a3cf54dc427cae5ee63e311ac647dcbc
Marcin Suder <marcin.code.github@gmail.com>
2026-02-26 01:57:56 +0100

    add method findById to get task

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,12 +1,26 @@
 package com.crud.tasks.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 @AllArgsConstructor
+@Entity(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
